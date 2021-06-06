7b34f70082b251973390b06a32b16e4349c2a568
h4mxa <duke80800@gmail.com>
2021-06-06 06:27:57 -0700

    init redux

--- src/App.js
+++ src/App.js
@@ -3,34 +3,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AddPhotoPage from "Pages/AddPhoto";
 import HomePage from "Pages/Home";
 
+import { createStore } from "redux";
+import postReducer from "reducers/index";
+
+const store = createStore(postReducer);
+
 class App extends Component {
   constructor() {
     // initialize state of components
     super();
-    this.state = {
-      posts: [
-        {
-          id: 0,
-          description: "beautiful landscape",
-          imageLink:
-            "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-            "3919321_1443393332_n.jpg",
-        },
-        {
-          id: 1,
-          description: "Aliens???",
-          imageLink:
-            "https://img.purch.com/rc/640x415/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA3Mi84NTEvb3JpZ2luYWwvc3BhY2V4LWlyaWRpdW00LWxhdW5jaC10YXJpcS1tYWxpay5qcGc=" +
-            "08323785_735653395_n.jpg",
-        },
-        {
-          id: 2,
-          description: "On a vacation!",
-          imageLink:
-            "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg",
-        },
-      ],
-    };
+
     this.removePhoto = this.removePhoto.bind(this);
     this.addPhoto = this.addPhoto.bind(this);
   }
